8e3412079e3e459e49d8fdf90ec4adb45cdc1423
Mahfuz <aamahfuz.pro@gmail.com>
2025-06-18 23:22:12 +0600

    Blog: added route

---
+++ src/app/modules/Blog/blog.route.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../User/user.constant';
+import { BlogControllers } from './blog.controller';
+import { BlogValidations } from './blog.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  validateRequest(BlogValidations.createBlogZodSchema),
+  BlogControllers.createBlog,
+);
+
+router.get('/', BlogControllers.getAllBlogs);
+
+router.get('/:id', BlogControllers.getSingleBlog);
+
+router.patch(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  validateRequest(BlogValidations.updateBlogZodSchema),
+  BlogControllers.updateBlog,
+);
+
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  BlogControllers.deleteBlog,
+);
+
+export const BlogRoutes = router;
